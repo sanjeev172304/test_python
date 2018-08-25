cb30987e2c493c927e3b17d445ad672b953f507b
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-08-25 22:16:11 +0530

    added 3 new html page

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-url('', views.index, name='home')
+    url(r'^$', views.HomePageView.as_view(), name='home'), # Notice the URL has been named
+    url(r'^about/$', views.AboutPageView.as_view(), name='about'),
+    url(r'^contact/$', views.ContactPageView.as_view(), name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView # Import TemplateView
 
-def index(request):
-    return HttpResponse("<h1>--- this is the home page </h1>")
+#def index(request):
+    #return HttpResponse("<h1>--- this is the home page </h1>")
 
+# Add the two views we have been talking about  all this time :)
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class ContactPageView(TemplateView):
+    template_name = "contact.html"
